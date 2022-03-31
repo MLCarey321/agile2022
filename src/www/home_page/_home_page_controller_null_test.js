4e25449176fe03dc1af21b5248b0c8621bbe5c81
James Shore <jshore@jamesshore.com>
2022-03-31 12:29:54 -0700

    Improve challenge text

--- src/www/home_page/_home_page_controller_null_test.js
+++ src/www/home_page/_home_page_controller_null_test.js
@@ -18,7 +18,8 @@ describe.only("Home Page Controller", () => {
 		it("GET renders home page", async () => {
 			/* CHALLENGE #1: Using nullable infrastructure
 			 *
-			 * We're going to start off nice and easy.
+			 * We're going to start off nice and easy. For this first challenge, all you need to do is make the
+			 * HomePageController return the home page when it receives a GET request.
 			 *
 			 * Hints:
 			 *
@@ -52,55 +53,68 @@ describe.only("Home Page Controller", () => {
 		it("POST asks ROT-13 service to transform text", async () => {
 			/* CHALLENGE #2: Tracking requests
 			 *
-			 * This is a lot more challenging! There's three parts to this challenge. Be sure to take it in small steps.
-			 *   a. Calling the ROT-13 service with hard-coded data
-			 *   b. Calling the ROT-13 service with the correct port
-			 *   c. Parsing the form data and calling the ROT-13 service with the correct text
+			 * This is a lot more challenging! You need to make HomePageController respond to POST requests. When it receives
+			 * the request, it needs to parse the form data and call the ROT-13 service. (You don't need to worry about
+			 * returning the home page for this challenge.)
+			 *
+			 * To make this challenge easier, take it in small steps. Be sure to test-drive each step.
+			 *   a. TCall the ROT-13 service with a hard-coded port and text
+			 *   b. Call the ROT-13 service with the correct port
+			 *   c. Parse the form data and call the ROT-13 service with the correct text
+			 *
+			 * Don't worry about parsing errors or edge cases for this challenge.
 			 *
 			 * Hints:
 			 *
-			 * 1. Your production code will need to call the ROT-13 service. The way it does so is with Rot13Client.
-			 * When you called HomePageController.createNull() in the last challenge, it created the Rot13Client for you.
-			 * But in this challenge, your test will need to call methods on Rot13Client, so you'll need to create the
-			 * Rot13Client manually and pass it into HomePageController.createNull(). The way to do so is with an optional
+			 * PART (a)
+			 *
+			 * 1. Your production code will need to call the ROT-13 service. The way to do so is with Rot13Client, which
+			 * is in "this._rot13Client".
+			 *
+			 * 2. When you called HomePageController.createNull() in the last challenge, it created the Rot13Client for you.
+			 * But in this challenge, you’ll need access to the Rot13Client instance in your test, so you'll need to create
+			 * it in your test and pass it into HomePageController. The way to do so is with an optional
 			 * parameter: HomePageController.createNull({ rot13Client }).
 			 *
-			 * 2. Rot13Client can be created with .createNull() just like everything else.
+			 * 3. To create Rot13Client, you can call Rot13Client.createNull().
 			 *
-			 * 3. You need to see which requests have been made to the ROT-13 service. You can track requests by calling
+			 * 4. Your test needs to see which requests have been made to the ROT-13 service. You can do that by calling
 			 * rot13Client.trackRequests(). It returns an array that is updated every time a new request is made. Note that
-			 * you have to call trackRequests() *before* making the request.
+			 * you have to call trackRequests() BEFORE making the request.
 			 *
-			 * 4. Compare the results of trackRequests() to your expected requests using assert.deepEqual(). Note that
+			 * 5. Compare the results of trackRequests() to your expected requests using assert.deepEqual(). Note that
 			 * trackRequests() will return an array of objects. Each object represents a single request. For example:
 			 *   assert.deepEqual(rot13Requests, [{
 			 *     port: 123,           // The port of the ROT-13 service
 			 *     text: "some text",   // The text sent to the service
 			 *   }]);
 			 *
-			 * 5. Once you have a simple test written, you can call the ROT-13 service in your production code by calling
+			 * 6. Once you have a simple test written, you can call the ROT-13 service in your production code by calling
 			 * this._rot13Client.transform(port, text). "Port" is the port that the ROT-13 server is running on.
-			 * "Text" is the text you want to transform. Start by hardcoding both of them. Don't worry about the return
-			 * value... that's for the next challenge.
+			 * "Text" is the text you want to transform. Start by hardcoding both of them. You don't need to worry about
+			 * the return value for this challenge.
 			 *
-			 * 6. Once the test is passing, change it to use the real ROT-13 port. The port is provided by WwwConfig,
-			 * specifically config.rot13ServicePort. Your test will need to set it up by passing the port into createNull().
-			 * As with all createNull() factories, it uses an optional parameter: WwwConfig.createNull({ rot13ServicePort }).
+			 * PART (b)
 			 *
-			 * 7. After you get the port working, you'll need to use the real text. That comes from user's input on the web
+			 * 7. Once the test is passing, change it to use the real ROT-13 port. The port is provided by WwwConfig,
+			 * specifically config.rot13ServicePort. Your test will need to set it up by passing the port into
+			 * WwwConfig.createNull(). As with all createNull() factories, it uses an optional parameter. Like this:
+			 *    WwwConfig.createNull({ rot13ServicePort }).
+			 *
+			 * PART (c)
+			 *
+			 * 8. After you get the port working, you'll need to use the real text. That comes from user's input on the web
 			 * page. Specifically, the web browser will send a request body with URL-encoded form data. It will look like
 			 * this: "text=hello%20world".
 			 *
-			 * 8. In your test, you can set up the request body by using--you guessed it--HttpRequest.createNull({ body }).
+			 * 9. In your test, you can set up the request body by using--you guessed it--HttpRequest.createNull({ body }).
 			 *
-			 * 9. In your production code, you can read the request body by using "await request.readBodyAsync()".
+			 * 10. In your production code, you can read the request body by using "await request.readBodyAsync()".
 			 *
-			 * 10. To parse the form data, use the standard "URLSearchParams" class. It works like this:
+			 * 11. To parse the form data, use the standard "URLSearchParams" class. It works like this:
 			 *   const formData = new URLSearchParams(requestBody);   // parse the request body
 			 *   const textFields = formData.getAll("text");  // get an array containing the value of all "text" fields
 			 *   const userInput = textFields[0];
-			 *
-			 * 11. Don't worry about parsing errors or edge cases. That's for a later challenge.
 			 *
 			 */
 
@@ -116,8 +130,15 @@ describe.only("Home Page Controller", () => {
 		it("POST renders result of ROT-13 service call", async() => {
 			/* CHALLENGE #3: Configuring responses
 			 *
-			 * This should be easier. It's similar to the last challenge, except this time we're dealing with the
-			 * response from the ROT-13 service, rather than checking that we're calling the ROT-13 service correctly.
+			 * This should be easier. It's a continuation of the last challenge. In the last challenge, you made the code
+			 * call the ROT-13 server. In this challenge, you need to make it return the correct home page response.
+			 * Specifically, the controller should return a web page with the translated ROT-13 string in the text field.
+			 *
+			 * Don't worry about server errors or edge cases for this challenge.
+			 *
+			 * When this challenge is complete, the code should work end to end. Check it manually as follows:
+			 *    1. Run `serve_dev 5010 5011` (Windows) or `./serve_dev.sh 5010 5011` (Mac/Linux) from the command line
+			 *    2. Access the page in a web browser: http://localhost:5010
 			 *
 			 * Hints:
 			 *
@@ -126,26 +147,22 @@ describe.only("Home Page Controller", () => {
 			 * 2. But this time, you'll need to control what data the Rot13Client returns. You can do that by providing
 			 * an array of objects, like this:
 			 *    Rot13Client.createNull([{ response: "my_response" }]);
+			 *
 			 * That tells the Rot13Client to respond with "my_response" the first time it's called. (If you wanted to
 			 * control additional responses, you can add more objects to the array.)
 			 *
 			 * 3. In your test's assertion, you'll need to check that postAsync returns the correct HttpResponse. It's just
-			 * like the GET test, except you want to render the response into the page. You can do that by calling
+			 * like challenge #1, except you want to render the response into the page. You can do that by calling
 			 * homePageView.homePage("my_response").
 			 *
-			 * 3. In the production code, you'll need to get the response from Rot13Client. It returns an object with a
+			 * 4. In the production code, you'll need to get the response from Rot13Client. It returns an object with a
 			 * transformPromise, like this:
 			 *    const { transformPromise } = this._rot13Client.transform(port, text);
 			 *
-			 * 4. You'll need to await the transformPromise to get the transformed text. Like this:
+			 * 5. You'll need to await the transformPromise to get the transformed text. Like this:
 			 *    const output = await transformPromise;
 			 *
-			 * 5. Once you have the output, you'll need to render the HttpResponse, just like for the GET challenge.
-			 *
-			 * 6. Don't worry about errors or edge cases. That's for a later challenge.
-			 *
-			 * 7. Once this tests pass, the code should work. Run "serve_dev 5010 5011" from the command-line and try
-			 * accessing the page in a web browser: http://localhost:5010
+			 * 6. Once you have the output, you'll need to render the HttpResponse, just like for the GET challenge.
 			 *
 			 */
 
@@ -174,10 +191,8 @@ describe.only("Home Page Controller", () => {
 			 * 1. You can use essentially the same test code as challenge #2, just with a different request body. Depending
 			 * on how you implemented challenge #2, the production code may work as-is.
 			 *
-			 * 2. Start thinking about how to factor out duplication, but don't implement it yet.
-			 *
-			 * 3. Instead of using beforeEach(), consider a helper method instead, such as "simulatePostAsync()". But
-			 * don't implement anything yet.
+			 * 2. Start thinking about how to factor out duplication, but don't implement it yet. Instead of using
+			 * beforeEach(), consider a helper method instead, such as "simulatePostAsync()". But don't refactor just yet.
 			 */
 
 			// Your test here.
@@ -186,8 +201,9 @@ describe.only("Home Page Controller", () => {
 		it("logs warning when form field not found (and treats request like GET)", async () => {
 			/* CHALLENGE #5: Logging
 			 *
-			 * This is similar to challenge #2, except that you're looking at log output rather than ROT-13 service requests.
-			 * Use an empty request body.
+			 * This is also similar to challenge #2, except now you need to handle a missing form field and log a warning.
+			 * Use an empty request body in your test and confirm that the controller outputs a log message and returns
+			 * the home page.
 			 *
 			 * Hints:
 			 *
@@ -218,12 +234,12 @@ describe.only("Home Page Controller", () => {
 		it("logs warning when duplicated form field found (and treats request like GET)", async () => {
 			/* CHALLENGE #6: Refactoring
 			 *
+			 * This is similar to the last challenge, in that you need to handle bad form data and log a warning. Use
+			 * this request body: "text=one&text=two"
+			 *
 			 * Before writing this test, look at your existing tests. There's probably a lot of duplication. Think about
 			 * how to refactor it to eliminate the duplication. Then, after the test is working, look at your production
 			 * code and find ways to clean it up.
-			 *
-			 * Use this request body:
-			 *    const body = "text=one&text=two";
 			 *
 			 * Hints:
 			 *
@@ -244,7 +260,7 @@ describe.only("Home Page Controller", () => {
 		it("fails gracefully, and logs error, when service returns error", async () => {
 			/* CHALLENGE #7: Service errors
 			 *
-			 * Test that the code handles errors in the ROT-13 service gracefully. Log the error and return the home
+			 * Make sure the code handles errors in the ROT-13 service gracefully. Log the error and return the home
 			 * page with "ROT-13 service failed" in the text field.
 			 *
 			 * Hints:
@@ -254,7 +270,7 @@ describe.only("Home Page Controller", () => {
 			 *    const rot13Client = Rot13Client.createNull([{ error: "my_error" }]);
 			 *
 			 * 2. When the Rot13Client encounters an error, it will throw an exception. Your production code will need to
-			 * catch the exception and log the error. I would use "log.emergency()" in this case.
+			 * catch the exception and log the error. You can use "log.emergency()" to do so.
 			 *
 			 * 3. Don't forget to check that the controller returns the correct home page. You don't need to do anything
 			 * fancy; just put the error in the text field, like this:
@@ -268,10 +284,20 @@ describe.only("Home Page Controller", () => {
 		it("fails gracefully, cancels request, and logs error, when service responds too slowly", async () => {
 			/* CHALLENGE #8: Timeouts
 			 *
-			 * The final challenge! This is a tough one. Test that the code handles timeouts in the ROT-13 service gracefully.
-			 * Log the error, cancel the request, and return the home page with "ROT-13 service timed out" in the text field.
+			 * The final challenge! This is a tough one. Make the code handle timeouts in the ROT-13 service. Log the
+			 * error, cancel the request, and return the home page with "ROT-13 service timed out" in the text field.
+			 *
+			 * To make this challenge easier, take it in small steps. Be sure to test-drive each step.
+			 *   a. Log the error and return the home page
+			 *   b. Cancel the request
+			 *
+			 * If you finish this challenge and still have time remaining, come up with your own challenges. One option is
+			 * to make the error handling more sophisticated, possibly with a customized response. You can do that by
+			 * adding a new function to HomePageView.
 			 *
 			 * Hints:
+			 *
+			 * PART (a)
 			 *
 			 * 1. Forcing the ROT-13 service to hang is just like forcing it to have an error. Pass a "hang" property, like
 			 * this:
@@ -286,17 +312,19 @@ describe.only("Home Page Controller", () => {
 			 * have to store the promise, advance the clock, and then await the promise. Like this:
 			 *    const responsePromise = controller.postAsync(request, config);
 			 *    await clock.advanceNullTimersAsync();
-			 *    await responsePromise;
+			 *    const response = await responsePromise;
 			 *
 			 * 4. In your production code, use Clock.timeoutAsync() to implement the timeout, like this:
 			 *    const output = await this._clock.timeoutAsync(timeoutInMs, transformPromise, timeoutFn);
+			 *
 			 * "timeoutInMs" is the number of milliseconds to wait before timing out.
 			 * "transformPromise" is the promise to wait for (in this case, the output of Rot13Client.transform)
 			 * "timeoutFn" is the function to call if the promise times out.
 			 *
-			 * 5. Use the above hints to implement the logging and graceful failure. Once that's working, implement the
-			 * request cancellation. In your tests, to check if the request was cancelled, you'll need to track requests
-			 * like in challenge #2. Cancellations are appended to the request array. A request followed by a cancellation
+			 * PART (b)
+			 *
+			 * 5. In your tests, to check if the request was cancelled, you'll need to track requests like in
+			 * challenge #2. Cancellations are appended to the request array. A request followed by a cancellation
 			 * looks like this:
 			 *    assert.deepEqual(rot13Requests, [{
 			 *      port: 9999,
@@ -314,10 +342,6 @@ describe.only("Home Page Controller", () => {
 			 *    cancelFn();   // cancels the request
 			 *
 			 * 7. After everything is working, be sure to look for opportunities to refactor the tests and production code.
-			 *
-			 * 8. If you finish this challenge and still have time, come up with your own challenges. One option is to
-			 * make the error handling more sophisticated, possibly with a customized response. You can do that by
-			 * adding a new function to HomePageView.
 			 *
 			 */
 
